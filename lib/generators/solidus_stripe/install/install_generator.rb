ba7a75087a6aa18997dedbacef77ef18ed14f71c
Alberto Vena <kennyadsl@gmail.com>
2019-04-05 22:33:59 +0200

    Allow creating seeds with the install command
    This commits allows to call
    
    bundle exec rails g solidus_stripe:install --auto_run_seeds=true --stripe_seeds_secret_key=xxx --stripe_seeds_publishable_key=yyy
    
    to run the engine installation with seeds auto ran. This could be useful
    in the to allow using stripe in the core sandbox.
    
    The migrate option has been added since in the context of the sandbox
    we don't want to run any migration, they are already ran with the
    spree:install --auto-accept command.

--- lib/generators/solidus_stripe/install/install_generator.rb
+++ lib/generators/solidus_stripe/install/install_generator.rb
@@ -1,18 +1,35 @@
+# frozen_string_literal: true
+
 module SolidusStripe
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      class_option :migrate, type: :boolean, default: true
       class_option :auto_run_migrations, type: :boolean, default: false
+      class_option :auto_run_seeds, type: :boolean, default: false
+      class_option :stripe_seeds_secret_key, type: :string
+      class_option :stripe_seeds_publishable_key, type: :string
 
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=solidus_stripe'
       end
 
       def run_migrations
-        if running_migrations?
+        if options.migrate? && running_migrations?
           run 'bundle exec rake db:migrate'
         else
           puts "Skiping rake db:migrate, don't forget to run it!"
         end
+      end
+
+      def populate_seed_data
+        return unless options.auto_run_seeds?
+
+        say_status :loading, "stripe seed data"
+        rake_options = []
+        rake_options << "STRIPE_SEEDS_SECRET_KEY=#{options[:stripe_seeds_secret_key]}" if options[:stripe_seeds_secret_key]
+        rake_options << "STRIPE_SEEDS_PUBLISHABLE_KEY=#{options[:stripe_seeds_publishable_key]}" if options[:stripe_seeds_publishable_key]
+
+        rake("db:seed:solidus_stripe #{rake_options.join(' ')}")
       end
 
       private
